d5f47ae89f0fff0b6bacffbc7bdf920f78f366ac
Vivek Maurya <Vivek563maurya@gmail.com>
2023-01-11 23:34:36 +0530

    final commit

--- 08 Context-reducer-todo/src/Components/Todos.js
+++ 08 Context-reducer-todo/src/Components/Todos.js
@@ -6,15 +6,29 @@ import { TodoContext } from "../context/TodoContext";
 import { REMOVE_TODO } from "../context/action.type";
 
 const Todos = () => {
-  const { todos, dispatch } = useContext(TodoContext);
-
-  return (
-    <ListGroup className="mt-5 mb-2 items">
-      {todos.map((todo) => (
-        <ListGroupItem key={todo.id}>{todo.todoString}</ListGroupItem>
-      ))}
-    </ListGroup>
-  );
-};
-
-export default Todos;
+    const { todos, dispatch } = useContext(TodoContext);
+  
+    return (
+      <ListGroup className="mt-5 mb-2 items">
+        {todos.map(todo => (
+          <ListGroupItem key={todo.id}>
+            {todo.todoString}
+            <span
+              className="float-end"
+              onClick={() => {
+                dispatch({
+                  type: REMOVE_TODO,
+                  payload: todo.id
+                });
+              }}
+            >
+              <FaCheckDouble />
+            </span>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    );
+  };
+  
+  export default Todos;
+  
