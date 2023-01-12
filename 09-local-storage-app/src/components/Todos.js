8cecd3be7b0e063eeae86506f6e2e117a5726125
Vivek Maurya <Vivek563maurya@gmail.com>
2023-01-13 01:18:28 +0530

    final commit

---
+++ 09-local-storage-app/src/components/Todos.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {ListGroup, ListGroupItem} from "reactstrap"
+import {FaCheckDouble} from "react-icons/fa"
+
+const Todos = ({ todos, markComplete }) => {
+    return (
+      <ListGroup className="mt-5 mb-2 items">
+        {todos.map(todo => (
+          <ListGroupItem key={todo.id}>
+            {todo.todoString}
+            <span className="float-end" onClick={() => markComplete(todo.id)}>
+              <FaCheckDouble />
+            </span>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    );
+  };
+  
+  export default Todos;
+  
