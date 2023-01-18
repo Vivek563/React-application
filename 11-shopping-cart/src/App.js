aa155f44e81fbd3968a8792c49f45c003f9bc258
Vivek Maurya <Vivek563maurya@gmail.com>
2023-01-19 00:24:14 +0530

    "final commit"

--- 11-shopping-cart/src/App.js
+++ 11-shopping-cart/src/App.js
@@ -1,49 +1,57 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "react-toastify/dist/ReactToastify.css"
-import { toast } from 'react-toastify';
-import BuyPage from './components/BuyPage';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
+import { Container, Row, Col } from "reactstrap";
+import { ToastContainer, toast } from "react-toastify";
+
+import BuyPage from "./components/BuyPage";
+import Cart from "./components/Cart";
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
 
-const [cartItem, setCardItem] = useState([])
+  const addInCart = item => {
+    const isAlreadyAdded = cartItem.findIndex(function(array) {
+      return array.id === item.id;
+    });
 
-const addInCard = item => {
-  const isAlreadyAdded = cardItem.findIndex(function(array){
-    return array.id === item.id
-  })
+    if (isAlreadyAdded !== -1) {
+      toast("already added in cart", {
+        type: "error"
+      });
+      return;
+    }
 
-  if (isAlreadyAdded !== -1) {
-    toast("already add in card",{
-      type:"error"
-    })
-  }
+    setCartItem([...cartItem, item]);
+  };
 
-  setCardItem([...cartItem, item])
-}
+  const buyNow = () => {
+    setCartItem([]);
 
+    toast("Purchase Complete", {
+      type: "success"
+    });
+  };
 
-const buyNow = () => {
-  setCardItem([])
-
-  toast("Purchase Complete",{
-    type:'success'
-  });
-};
-
-const removeItem = item => {
-  setCardItem(cartItem.filter(singleItem.id !== item.id))
-};
-
-
+  const removeItem = item => {
+    setCartItem(cartItem.filter(singleItem => singleItem.id !== item.id));
+  };
 
   return (
-    <div className="App">
-     <BuyPage addInCart={addInCart}/>
-    </div>
+    <Container fluid>
+      <ToastContainer />
+      <Row>
+        <Col md="8">
+          <BuyPage addInCart={addInCart} />
+        </Col>
+        <Col md="4">
+          <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- 11-shopping-cart/src/components/BuyPage.js
+++ 11-shopping-cart/src/components/BuyPage.js
@@ -1,59 +1,56 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import CartItem from "./CartItem";
 
-import {faker,random, commerce} from "faker-js/faker"
-import {Container, col, Row} from "reactstrap"
+import { random, commerce } from "faker";
+import { Container, Col, Row } from "reactstrap";
 
+const apiKey = "INSET_YOUR_KEY_HERE";
 
-const apikey = "INSERT_YOUR_KEY_HERE"
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+const localurl = "https://api.jsonserve.com/RSgGku";
+const BuyPage = ({ addInCart }) => {
+  const [product, setProduct] = useState([]);
 
-const localurl = "https://api.jsonserve.com/KBonX0"
+  //   const fetchPhotos = async () => {
+  //     const response = await Axios.get(url, {
+  //       header: {
+  //         Authorization: apiKey
+  //       }
+  //     });
 
-const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1"
+  const fetchPhotos = async () => {
+    const { data } = await Axios.get(localurl, {});
 
-const BuyPage = ({addInCart}) => {
-    const [product, setProduct] = useState([])
+    const { photos } = data;
 
-// const fetchPhotos = async () => {
-//     const response = await Axios.get(url, {
-//         header: {
-//             Authorization:apiKey
-//         }
-//     });
+    const allProduct = photos.map(photo => ({
+      smallImage: photo.src.medium,
+      tinyImage: photo.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid()
+    }));
 
+    setProduct(allProduct);
+  };
 
-const fetchPhotos = async () => {
-    const {data} = await Axios.get(localurl); 
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
 
-const {photos} = data;
-
-const allProduct = photos.map(photo => ({
-    smallImage:photo.src.medium,
-    tinyImage:photo.src.tiny,
-    productName: random.word(),
-    productPrice: commerce.price(),
-    id: faker.datatype.uuid(),
-}));
-
-setProduct(allProduct);
-
-};
-
- 
-useEffect(() => {
-    fetchPhotos()
-},[])
-
-return(
+  return (
     <Container fluid>
-        <h1 className="text-success text-center">Buy page</h1>
-        <Row>
-            {product.map(product => (
-                <span key = {product.id}>{product.productName}</span>
-            ))}
-        </Row>
+      <h1 className="text-success text-center">Buy Page</h1>
+      <Row>
+        {product.map(product => (
+          <Col md={4} key={product.id}>
+            <CartItem product={product} addInCart={addInCart} />
+          </Col>
+        ))}
+      </Row>
     </Container>
-)
-}
+  );
+};
 
 export default BuyPage;
