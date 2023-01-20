4f3fa4896331b5315a77a8debc3df48e65b49996
Vivek Maurya <Vivek563maurya@gmail.com>
2023-01-21 01:20:57 +0530

    "NavBar updated"

--- 13-firebase-github-api/src/layout/Header.js
+++ 13-firebase-github-api/src/layout/Header.js
@@ -7,7 +7,7 @@ import {
   Nav,
   NavItem,
   NavLink,
-  NavbarText
+  NavbarText,
 } from "reactstrap";
 
 import { Link } from "react-router-dom";
@@ -15,6 +15,14 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 const Header = () => {
+
+    const context = useContext(UserContext)
+
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => setIsOpen(!isOpen)
+
   return (
     <Navbar color="info" light expand="md">
       <NavbarBrand>
@@ -22,18 +30,31 @@ const Header = () => {
           LCO gitfire app
         </Link>
       </NavbarBrand>
-      <NavbarToggler />
-      <Collapse navbar>
+      <NavbarText className="text-white">{
+        context.user?.email ? context.user.email : ""
+      }</NavbarText>
+      <NavbarToggler onClick={toggle}/>
+      <Collapse isOpen={isOpen} navbar>
         <Nav className="ms-auto" navbar>
-          <NavItem>
-            <NavLink className="text-white">Signup</NavLink>
+          {
+            context.user ? (<NavItem>
+                <NavLink tag={Link} to="/" className="text-white">
+                  Logout
+                </NavLink>
+              </NavItem>) : ( 
+              <>
+              <NavItem>
+            <NavLink tag={Link} to="/" className="text-white">Signin</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink className="text-white">Signin</NavLink>
+            <NavLink tag={Link} to="/" className="text-white">
+              Signup</NavLink>
           </NavItem>
-          <NavItem>
-            <NavLink className="text-white">Logout</NavLink>
-          </NavItem>
+          </>
+          )
+          }
+         
+          
         </Nav>
       </Collapse>
     </Navbar>
